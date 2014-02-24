757fb35b419c9b7630c9d24e187903ca155e4fa3
Bryan Ma <bryanhma@gmail.com>
2014-02-24 15:01:51 -0500

    texture edits, space station doesn't move, reticle doesn't collide

--- Spacewalk/Assets/Rotate.cs
+++ Spacewalk/Assets/Rotate.cs
@@ -11,7 +11,7 @@ public class Rotate : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 
-		transform.Rotate(new Vector3(0,0,.07f));
+		transform.Rotate(new Vector3(0,0,.005f));
 	
 	}
 }
