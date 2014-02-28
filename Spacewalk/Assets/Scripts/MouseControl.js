925407a4b9bb4036dc91de2c89ad0758c9f221c8
gianordoli <gianordoligabriel@yahoo.com.br>
2014-02-28 13:29:33 -0500

    Testing new MouseControl
    Testing a mouse control that is relative to camera.

--- Spacewalk/Assets/Scripts/MouseControl.js
+++ Spacewalk/Assets/Scripts/MouseControl.js
@@ -8,11 +8,15 @@ function Start () {
 	verticalSpeed = 2.0;
 }
  
-function Update (){
+function FixedUpdate (){
     // Get the mouse delta. This is not in the range -1...1
  
     var x : float = - horizontalSpeed * Input.GetAxis ("Mouse X");
     var y : float = - verticalSpeed * Input.GetAxis ("Mouse Y");
  
     transform.Translate (y, x, 0);
+//	rigidbody.AddForce(x, 0, y);
+//	var localForce : Vector3 = camera.TransformDirection(new Vector3(x, 0, y));
+//	localForce.y = 0;
+//	rigidbody.AddForce(localForce);
 }
