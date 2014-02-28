e9ba61efa32d14b36770cae442d60ed21be2730e
gianordoli <gianordoligabriel@yahoo.com.br>
2014-02-28 13:44:53 -0500

    MouseControl on arm!
    Just a cylinder, actually...

--- Spacewalk/Assets/Scripts/MouseControl.js
+++ Spacewalk/Assets/Scripts/MouseControl.js
@@ -4,17 +4,18 @@ var horizontalSpeed : float;
 var verticalSpeed : float;
 
 function Start () {
-	horizontalSpeed = 2.0;
-	verticalSpeed = 2.0;
+	horizontalSpeed = 2;
+	verticalSpeed = 2;
 }
  
 function FixedUpdate (){
     // Get the mouse delta. This is not in the range -1...1
  
-    var x : float = - horizontalSpeed * Input.GetAxis ("Mouse X");
-    var y : float = - verticalSpeed * Input.GetAxis ("Mouse Y");
+    var y : float = - horizontalSpeed * Input.GetAxis ("Mouse X");
+    var x : float = - verticalSpeed * Input.GetAxis ("Mouse Y");
  
-    transform.Translate (y, x, 0);
+//    transform.Translate (x, 0, y);
+transform.Rotate (x, 0, y);
 //	rigidbody.AddForce(x, 0, y);
 //	var localForce : Vector3 = camera.TransformDirection(new Vector3(x, 0, y));
 //	localForce.y = 0;
